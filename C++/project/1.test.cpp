aeb820b12de446862d6543f82ab330abde28a963
MrsmallLi <k952314934@gmail.com>
2024-08-14 16:47:35 +0800

    测试

---
+++ C++/project/1.test.cpp
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: 1.test.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Wed 14 Aug 2024 04:46:59 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+
+int main() {
+
+
+    return 0;
+}
