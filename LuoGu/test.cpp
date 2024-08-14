b3acb635ffb0e5fbc3ce89c3ac4d55b21dc4b7f4
MrsmallLi <k952314934@gmail.com>
2024-08-14 16:56:18 +0800

    更新

---
+++ LuoGu/test.cpp
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: test.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Wed 14 Aug 2024 04:54:24 PM CST
+ ************************************************************************/
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+
+int main() {
+    int a;
+    scanf("%d", &a);
+    printf("%d\n", a);
+    return 0;
+}
